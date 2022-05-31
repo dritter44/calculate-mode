f4488c1f137ba387703f6ab489343946edbe026c
dritter44 <dreither44@gmail.com>
2022-05-30 19:29:25 -0500

    can't get calculateModeSpec to work. telling me that calculateMode is not a function

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,48 @@
-const calculateMode = () => {
+let entryCountObj ={}
+let max = 0
+let ansArr = []
+
+const genObj = (arr) => {
+    arr.forEach(element => {
+        //console.log(element)
+        if(entryCountObj.hasOwnProperty(element)){
+            entryCountObj[element] += 1
+            //console.log(ansObject)
+        } else{
+            entryCountObj[element] = 1
+        }
+    });
+    //console.log(entryCountObj)
+    return entryCountObj
+}
+
+//next step is to determine the max value of values { '1': 2, '2': 2 }
+const getMax = (entryCountObj) => {max = Math.max(... Object.values(entryCountObj))
+    return max
+}
+//console.log(getMax({ '1': 2, '2': 2 }))
+
+
+const calculateMode = (arr) => {
+    //let max = 0
+    //let entryCountObj ={}
+    //genObj(arr)
+    getMax(genObj(arr))
+    //console.log(getMax(genObj(arr)))
+    //console.log(entryCountObj)
+    //getMax(entryCountObj)
+    //console.log(max)
+    for(let i = 0; i < Object.values(entryCountObj).length; i++){
+        //console.log(Object.values(entryCountObj))
+        if(Object.values(entryCountObj)[i] == max){
+            ansArr.push(Object.keys(entryCountObj)[i])
+        }
+    }
+    //console.log(max,ansArr,entryCountObj)
+    return ansArr
+
+
+// first step will be to generate an object with the array entries as keys, and count of entries as values
 
 }
+//console.log(calculateMode(["fart","poop", "fart", "fart", "queef","queef","queef"]))
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,4 @@
 // Write your own unit tests here!
+var mode = require("./calculateMode");
+let result = mode.calculateMode(["fart","poop", "fart", "fart", "queef","queef","queef"])
+console.log(result);
